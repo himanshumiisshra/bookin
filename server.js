662df6b1fe3e86996fdc33648238a044cc66ef28
Himanshu Mishra <76582642+himanshumiisshra@users.noreply.github.com>
2023-08-07 17:38:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+app.use(express.json());
+const userRoute = require("./routes/userRoute");                     
+const adminRoute = require("./routes/adminRoute");
+const doctorRoute = require("./routes/doctorRoute");
+const path = require("path");
+const cors = require("cors")
+
+app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/doctor", doctorRoute);
+
+
+
+const port = process.env.PORT || 5000;
+app.use(cors())
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Node Express Server Started at ${port}!`));
