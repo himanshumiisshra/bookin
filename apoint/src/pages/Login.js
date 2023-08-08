a0d9f767f7c40975424b5acd9acee5174a8de295
Himanshu Mishra <76582642+himanshumiisshra@users.noreply.github.com>
2023-08-08 23:16:44 +0530

    second commit

--- apoint/src/pages/Login.js
+++ apoint/src/pages/Login.js
@@ -12,7 +12,7 @@ function Login() {
   const onFinish = async (values) => {
     try {
       dispatch(showLoading());
-      const response = await axios.post("/api/user/login", values);
+      const response = await axios.post("https://bookib.onrender.com/api/user/login", values);
       dispatch(hideLoading());
       if (response.data.success) {
         toast.success(response.data.message);
